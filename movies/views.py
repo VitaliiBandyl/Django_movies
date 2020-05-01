b1d680cbff471fb4058791d92c3fd9d065a9ae16
VitaliiBandyl <vitaliibandyl@gmail.com>
2020-05-01 21:53:22 +0300

    Added HTML/CSS template and film list view

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.base import View
 
-# Create your views here.
+from .models import Movie
+
+
+class MoviesView(View):
+    """Views for Movies List"""
+    def get(self, request):
+        movie = Movie.objects.all()
+        return render(request, "movies/movies.html", context={"movie_list": movie})
