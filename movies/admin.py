bab3991f79c5d4c7cbb043091a8e03fc5b8806aa
VitaliiBandyl <vitaliibandyl@gmail.com>
2020-05-01 19:53:27 +0300

    Admin panel setup

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Actor, Genre, Movie, MovieShot, RatingStar, Rating, Reviews
+
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Actor)
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(MovieShot)
+admin.site.register(RatingStar)
+admin.site.register(Rating)
+admin.site.register(Reviews)
+
