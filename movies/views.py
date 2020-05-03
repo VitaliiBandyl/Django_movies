a0c502135372946222ffb5a3a853118934257865
VitaliiBandyl <vitaliibandyl@gmail.com>
2020-05-03 22:15:56 +0300

    Changed views for list and detail from Views to ListView and DetailView accordingly

--- movies/views.py
+++ movies/views.py
@@ -1,18 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
 from .models import Movie
 
 
-class MoviesView(View):
-    """Views for Movies List"""
-    def get(self, request):
-        movie = Movie.objects.all()
-        return render(request, "movies/movies.html", context={"movie_list": movie})
+class MoviesView(ListView):
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
 
 
-class MovieDetailView(View):
-    """Detailed movie description"""
-    def get(self, request, slug):
-        movie = Movie.objects.get(url=slug)
-        return render(request, 'movies/movie_detail.html', context={'movie': movie})
+class MovieDetailView(DetailView):
+    model = Movie
+    slug_field = 'url'
